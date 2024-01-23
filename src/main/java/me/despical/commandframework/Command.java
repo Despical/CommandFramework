56da90930ce55c24995b851d81afd27ad93bd24d
Berke Akçen <berkeakcen@gmail.com>
2024-01-23 13:18:38 +0300

    Updated license header

--- src/main/java/me/despical/commandframework/Command.java
+++ src/main/java/me/despical/commandframework/Command.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2021 - Despical
+ * Command Framework - Annotation based command framework
+ * Copyright (C) 2024  Berke Akçen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package me.despical.commandframework;
--- src/main/java/me/despical/commandframework/utils/Utils.java
+++ src/main/java/me/despical/commandframework/utils/Utils.java
@@ -1,6 +1,6 @@
 /*
- * Commons - Box of the common utilities.
- * Copyright (C) 2023 Despical
+ * Command Framework - Annotation based command framework
+ * Copyright (C) 2024  Berke Akçen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -9,11 +9,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package me.despical.commandframework.utils;
