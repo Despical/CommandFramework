a722e72c6908d82e5ecb5ce986baf690cbf3ec64
Berke Akçen <berkeakcen@gmail.com>
2026-01-19 17:56:24 +0300

    Fixed parsing of multiple Flag and Option annotations

--- src/main/java/dev/despical/commandframework/parser/OptionParser.java
+++ src/main/java/dev/despical/commandframework/parser/OptionParser.java
@@ -34,93 +34,92 @@ import java.util.regex.Pattern;
 @ApiStatus.Internal
 public final class OptionParser {
 
-	private final Flag[] flags;
-	private final Option[] options;
-	private final Set<String> arguments;
-	private final Set<String> parsedFlags;
-	private final Map<String, List<String>> parsedOptions;
+    private final Flag[] flags;
+    private final Option[] options;
+    private final Set<String> arguments;
+    private final Set<String> parsedFlags;
+    private final Map<String, List<String>> parsedOptions;
 
-	public OptionParser(String[] arguments, Method method) {
-		this.flags = method.getAnnotationsByType(Flag.class);
-		this.options = method.getAnnotationsByType(Option.class);
-		this.arguments = new HashSet<>(Arrays.asList(arguments));
-		this.parsedFlags = new HashSet<>();
-		this.parsedOptions = new HashMap<>();
-	}
+    public OptionParser(String[] arguments, Method method) {
+        this.flags = method.getAnnotationsByType(Flag.class);
+        this.options = method.getAnnotationsByType(Option.class);
+        this.arguments = new HashSet<>(Arrays.asList(arguments));
+        this.parsedFlags = new HashSet<>();
+        this.parsedOptions = new HashMap<>();
+    }
 
-	public Map<String, List<String>> parseOptions() {
-		for (Option option : options) {
-			this.parseOption(option);
-		}
+    public Map<String, List<String>> parseOptions() {
+        for (Option option : options) {
+            this.parseOption(option);
+        }
 
-		return this.parsedOptions;
-	}
+        return this.parsedOptions;
+    }
 
-	public Set<String> parseFlags() {
-		for (Flag flag : flags) {
-			this.parseFlag(flag);
-		}
+    public Set<String> parseFlags() {
+        for (Flag flag : flags) {
+            this.parseFlag(flag);
+        }
 
-		return this.parsedFlags;
-	}
+        return this.parsedFlags;
+    }
 
-	private void parseOption(Option option) {
-		String prefix = option.prefix();
-		String keySeparator = Pattern.quote(option.keySeparator());
-		String valueSeparator = Pattern.quote(option.valueSeparator());
-		Iterator<String> iterator = arguments.iterator();
+    private void parseOption(Option option) {
+        String prefix = option.prefix();
+        String keySeparator = option.keySeparator();
+        String valueSeparator = option.valueSeparator();
 
-		while (iterator.hasNext()) {
-			String argument = iterator.next();
+        Iterator<String> iterator = arguments.iterator();
 
-			if (!argument.startsWith(prefix)) {
-				continue;
-			}
+        while (iterator.hasNext()) {
+            String argument = iterator.next();
 
-			if (option.allowSeparating() && !argument.contains(option.keySeparator())) {
-				continue;
-			}
+            if (!argument.startsWith(prefix)) {
+                continue;
+            }
 
-			String[] options = argument.substring(prefix.length()).split(keySeparator);
+            String raw = argument.substring(prefix.length());
 
-			if (!option.allowSeparating()) {
-				if (options.length < 2) {
-					continue;
-				}
+            if (!raw.startsWith(option.value() + keySeparator)) {
+                continue;
+            }
 
-				String value = options[1];
-				this.parsedOptions.put(option.value(), Collections.singletonList(value));
+            String valuePart = raw.substring((option.value() + keySeparator).length());
 
-				iterator.remove();
-				continue;
-			}
+            if (!option.allowSeparating()) {
+                this.parsedOptions.put(option.value(), Collections.singletonList(valuePart));
+            } else {
+                this.parsedOptions.put(
+                    option.value(),
+                    Arrays.asList(valuePart.split(Pattern.quote(valueSeparator)))
+                );
+            }
 
-			String[] values = options[1].split(valueSeparator);
-			this.parsedOptions.put(option.value(), Arrays.asList(values));
+            iterator.remove();
+            return;
+        }
+    }
 
-			iterator.remove();
-		}
-	}
+    private void parseFlag(Flag flag) {
+        String prefix = flag.prefix();
+        Iterator<String> iterator = this.arguments.iterator();
 
-	private void parseFlag(Flag flag) {
-		String prefix = flag.prefix();
+        while (iterator.hasNext()) {
+            String argument = iterator.next();
 
-		outer:
-		for (String argument : this.arguments) {
-			if (!argument.startsWith(prefix)) {
-				continue;
-			}
+            if (!argument.startsWith(prefix)) {
+                continue;
+            }
 
-			String foundFlag = argument.substring(prefix.length());
+            String foundFlag = argument.substring(prefix.length());
 
-			for (String flagName : flag.value()) {
-				if (!flagName.equals(foundFlag)) {
-					continue;
-				}
-
-				this.parsedFlags.add(flagName);
-				continue outer;
-			}
-		}
-	}
+            for (String flagName : flag.value()) {
+                if (flagName.equals(foundFlag)) {
+                    this.parsedFlags.add(flagName);
+                    iterator.remove();
+                    return;
+                }
+            }
+        }
+    }
 }
